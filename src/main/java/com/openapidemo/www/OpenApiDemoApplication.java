787544e5a3ace7ba7a70dc2417b5be7d35eea2f6
Srinivasan Ramu <srinimarva@gmail.com>
2022-03-20 14:13:53 -0500

    Add files via upload

---
+++ src/main/java/com/openapidemo/www/OpenApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.openapidemo.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenApiDemoApplication.class, args);
+	}
+
+}
